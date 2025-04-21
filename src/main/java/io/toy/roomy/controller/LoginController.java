60ee1e91a4707ed2ea7b2b3abbd1d17d14cc56a8
mommos <gjwnsqja45@naver.com>
2025-04-21 20:02:11 +0900

    feat: html 추가 및 컨트롤러 추가

---
+++ src/main/java/io/toy/roomy/controller/LoginController.java
@@ -0,0 +1,25 @@
+package io.toy.roomy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/signup")
+    public String getSignupPage() {
+        return "/login/signup";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+
+        model.addAttribute("title", "login/login :: title");
+        model.addAttribute("css", "login/login :: css");
+        model.addAttribute("content", "login/login :: content");
+
+        return "/layout/layout";
+    }
+
+}
