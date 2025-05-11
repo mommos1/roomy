a0b5eae831f19f4e126719b8a4e49ce5c96e1653
gjwns <gjwnsqja45@naver.com>
2025-05-11 23:37:45 +0900

    feat: 객실 상세페이지 추가

--- src/main/java/io/toy/roomy/controller/reservation/UserStayController.java
+++ src/main/java/io/toy/roomy/controller/reservation/UserStayController.java
@@ -11,11 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserStayController {
 
     /**
-     * 호텔, 모텔 의 리스트를 반환합니다
+     * 호텔, 모텔 의 리스트를 반환합니다.
      * @return 호텔, 모텔 리스트
      */
     @GetMapping("/list")
     public String getUserStayListPage(Model model) {
         return CommonUtil.commonModelLayout(model, "reservation/user/stayList");
+    }
+
+    /**
+     * 호텔, 모텔 의 상세 페이지를 반환합니다.
+     * @return 호텔, 모텔 상세페이지
+     */
+    @GetMapping("/info")
+    public String getUserStayInfoPage(Model model) {
+        return CommonUtil.commonModelLayout(model, "reservation/user/stayInfo");
     }
 }
